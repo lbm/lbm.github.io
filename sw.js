fbf1d9d5da0a0872dc57c80564e2fdf7b0b814ae
Lachlan Mitchell <lachlan.mitchell@icloud.com>
2021-02-09 21:57:45 +0900

    Fixed issue caused by Gatsby cache

--- sw.js
+++ sw.js
@@ -27,17 +27,17 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1b0c64bbe038aa49eba3.js"
+    "url": "webpack-runtime-bb54af278a67ec0bdc6e.js"
   },
   {
     "url": "framework-cd3e1e804d552fa282ef.js"
   },
   {
-    "url": "app-09b152ef1f9f7f54c7d1.js"
+    "url": "app-f7c0e8e76c79fe2c4211.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "14c72ac7e38308f4c0d44d56d4cfd761"
+    "revision": "81f5f1af1ddb5529808b0d22a3011c6d"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -139,7 +139,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-09b152ef1f9f7f54c7d1.js`))) {
+  if (!resources || !(await caches.match(`/app-f7c0e8e76c79fe2c4211.js`))) {
     return await fetch(event.request)
   }
 
